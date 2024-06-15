7b70df6aacd591519c592b5ec3741584700a9de8
Shirly Uy <162430149+shing2x@users.noreply.github.com>
2024-06-16 02:04:28 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -9,6 +9,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connections
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error); 
 }
 ?>
