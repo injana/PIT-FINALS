7c3814c0e99b455460903ceb0d69b054d61bd6d8
Shirly Uy <162430149+shing2x@users.noreply.github.com>
2024-06-16 02:03:15 +0800

    Update insert_notification.php

--- user/insert_notification.php
+++ user/insert_notification.php
@@ -7,7 +7,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $user_id = $data['user_id'];
 $message = $data['message'];
-
+ 
 $sql = "INSERT INTO notifications (user_id, message) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("is", $user_id, $message);
