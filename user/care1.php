07db8e865c640b55f0d1d79221b272f4f4baaa99
Shirly Uy <162430149+shing2x@users.noreply.github.com>
2024-06-16 02:02:05 +0800

    Update care1.php

--- user/care1.php
+++ user/care1.php
@@ -6,7 +6,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://kit.fontawesome.com/ecd5e25db3.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ecd5e25db3.js" crossorigin="anonymous"></script> 
     
     
   
