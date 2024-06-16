19a8669dde91ecce4203e3c35e251b72f60db6c4
jenneva13 <162430804+jenneva13@users.noreply.github.com>
2024-06-16 10:25:07 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ header('location: ../logout');
   <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.2/dist/full.min.css" rel="stylesheet" type="text/css" />
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <title>Pet For All</title>
+  <title>Pet For All</title> 
 </head>
 
 <body class="relative bg-yellow-50 overflow-hidden max-h-screen">
